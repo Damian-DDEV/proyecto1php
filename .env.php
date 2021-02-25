0e9906b453a45262cc16236a52ad556935b868c7
Damian-DDEV <60989776+Damian-DDEV@users.noreply.github.com>
2021-02-24 23:20:52 -0300

    Delete .env.php

--- .env.php
+++
@@ -1,9 +0,0 @@
-<?php
-function credenciales() {
-    return [
-        'usuario'=>'damian' ,
-        'clave'=>'ENTrZQDD7z4RDKk0',
-        'servidor'=>'localhost',
-        'base_de_datos'=>'tpfinal'
-    ];
-}
